1cb13367ed731ff20474264d2ee05569e31ec6a0
Guilherme <contato.guilhermealves@gmail.com>
2021-11-11 10:03:17 -0300

    Add files via upload

---
+++ quasarlista/README.md
@@ -0,0 +1,41 @@
+# TODO LIST
+
+I built this page to practice my frontend skills with Quasar and Vue.js.
+
+This is a page that allows users to add tasks to a todo list. 
+
+Users can delete their tasks.  
+
+Click here: [Demo](https://walter-clayton.github.io/quasartodolist/)
+
+![capture](./src/assets/todolist.gif)
+
+## Features
+
+* Add Tasks
+* Delete Tasks
+* Flash messages
+* User Interface with profile picture and tab bar
+
+## Built with
+
+* HTML5
+* SCSS
+* QUASAR
+* VUE.js
+
+## Instructions for contributors
+
+if you would like to contribute on GitHub :smiley:
+* QUASAR [instructions](quasar.md)
+* GitHub [instructions](contributions.md)
+
+## Author
+
+**Walter Clayton** 
+* :octocat: [GitHub Profile](https://github.com/walter-clayton)
+* :bowtie: [LinkedIn](https://www.linkedin.com/in/walter-clayton-2b50b4191/)
+
+## Copyright
+
+<p>&copy; 2019 Walter Clayton, All Rights Reserved<p>
---
+++ quasarlista/src/layouts/MyLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-purple-10 q-mx-none">
+        <div class=" q-py-xl q-sb-md q-mx-auto">
+          <div class="text-h3">
+            Dyndo - Lista de tarefas
+            <span v-if="$q.platform.is.ios">IOS</span>
+            <span v-else-if="$q.platform.is.mac">Mac</span>
+          </div>
+          <div class="text-subtitle q-py-sm q-mr-none">{{ todaysDate }}</div>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from "quasar";
+
+export default {
+  name: "MyLayout",
+
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  },
+  computed: {
+    todaysDate() {
+      let timeStamp = Date.now();
+      return date.formatDate(timeStamp, "dddd M MMMM YYYY");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.5;
+  filter: grayscale(100%);
+}
+</style>
